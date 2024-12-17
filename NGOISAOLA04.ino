84ecf794a7419d9d3fa6f39ef722e4e43098b77e
HuyHoangEngr <141493308+HuyHoangEngr@users.noreply.github.com>
2024-12-17 16:15:40 +0700

    #03. Sang nguoc chieu kim dong ho
    #03. Sang nguoc chieu kim dong ho (so lan va thoi gian delay hieu ung)

--- NGOISAOLA04.ino
+++ NGOISAOLA04.ino
@@ -23,12 +23,14 @@ void resetBienY(){
     Y[i]=1;
   }
 }
+
 void xuatTinhieu(){
   digitalWrite(A, Y[0]);   
   digitalWrite(B, Y[1]);
   digitalWrite(C, Y[2]);   
   digitalWrite(D, Y[3]);
 }
+
 void sangTheochieukimdongho(int solan, int thoigianchay){
   for(int vong=0;vong<solan;vong=vong+1){
       for(int i=0;i<4;i=i+1){
@@ -41,49 +43,22 @@ void sangTheochieukimdongho(int solan, int thoigianchay){
       delay(thoigianchay);
   }
 }
-void nhapnhay5lan(int solan, int thoigian){
-  for(int i=0; i<solan; i=i+1) {
-    digitalWrite(A, HIGH);   
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);   
-    digitalWrite(D, HIGH);
-    delay(thoigian);
-    digitalWrite(A, LOW);   
-    digitalWrite(B, LOW);
-    digitalWrite(C, LOW);   
-    digitalWrite(D, LOW);
-    delay(thoigian);
-    digitalWrite(A, HIGH);   
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);   
-    digitalWrite(D, HIGH);
-    delay(thoigian);
-  }
-  delay(2000); //chuyen hieu ung
-}
-void sangdantungcanh(int solan, int thoigian){
-  for(int i=0;i<solan;i=i+1){
-    digitalWrite(A, LOW);
-    delay(thoigian);
-    digitalWrite(B, LOW);
-    delay(thoigian);
-    digitalWrite(C, LOW);
-    delay(thoigian);
-    digitalWrite(D, LOW);
-    delay(thoigian);
-    digitalWrite(A, HIGH);   
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);   
-    digitalWrite(D, HIGH); 
+
+void sangNguocchieukimdongho(int solan, int thoigianchay){
+  for(int vong=0;vong<solan;vong=vong+1){
+      for(int i=0;i<4;i=i+1){
+        Y[3-i]=0;
+        xuatTinhieu();
+        delay(thoigianchay);
+      }
+      resetBienY();
+      xuatTinhieu();
+      delay(thoigianchay);
   }
 }
+
 void loop() {
-//  nhapnhay5lan(5, 2000);
-//  nhapnhay5lan(5, 1000);
-//  nhapnhay5lan(10, 500);
-//  nhapnhay5lan(20, 200);
-//  nhapnhay5lan(50, 100);
-//  nhapnhay5lan(100, 50);
-//  sangdantungcanh(50, 100);
-  sangTheochieukimdongho(5,100);
+//  nhapnhay(5, 2000);
+//  sangTheochieukimdongho(5,100);
+  sangNguocchieukimdongho(5,200);
 }
